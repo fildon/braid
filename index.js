64dae8afb004478d0ee06298bedac113232c8d3d
fildon <maxfmckay@gmail.com>
2024-07-30 16:21:12 +0000

    Deploying to gh-pages from @ fildon/braid@5fdfa23603b4aa10066f0ee94157b419c3a3c1de 🚀

---
+++ index.js
@@ -0,0 +1,11 @@
+(() => {
+  // src/index.ts
+  document.addEventListener("DOMContentLoaded", () => {
+    let braid = {
+      length: 10,
+      ropes: 10,
+      crossings: /* @__PURE__ */ new Set()
+    };
+    console.log("hello");
+  });
+})();
